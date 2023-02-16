f1560085fa3e4004c8fc407952a6e2bb7c20fa94
olivier <tuyishime.rwa@gmail.com>
2023-02-16 13:43:15 -0500

    print words to find

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Table table = new Table();
         table.printTable();
+        table.printWordsToFind();
     }
 }
--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,12 +1,15 @@
+import java.util.Random;
+
 public class Table {
     private static int tableWidth = 15;
     private static int tableHeigth = tableWidth;
     private int numOfWords = 35;
-    private Word[] wordsToFind = new Word[numOfWords];
+    private Word[] wordBank = new Word[numOfWords];
+    private Word[] wordsToFind;
     private HorizontalRow[] horizontalRows = new HorizontalRow[tableHeigth];
     private VerticalRow[] verticalRows = new VerticalRow[tableWidth];
     private DiagonalRow[] leftDiagonalRows = new DiagonalRow[tableHeigth * 2 - 1];
-    private DiagonalRow[] rightDiagonalRows = new DiagonalRow[tableHeigth];
+    private DiagonalRow[] rightDiagonalRows = new DiagonalRow[tableHeigth * 2 - 1];
 
     public Table() {
         for (int i = 0; i < tableHeigth; i++) {
@@ -17,7 +20,8 @@ public class Table {
         int doubleHeight = tableHeigth * 2;
         for (int i = 0; i < doubleHeight - 1; i++) {
             int size = -Math.abs(i - (doubleHeight - 2) / 2) + tableHeigth;
-            leftDiagonalRows[i] = new DiagonalRow(i, (int) size, 1);
+            leftDiagonalRows[i] = new DiagonalRow(i, size, 0);
+            rightDiagonalRows[i] = new DiagonalRow(i, size, 1);
         }
         generatewordsToFind();
         populate();
@@ -41,34 +45,59 @@ public class Table {
 
         for (int i = 0; i < numOfWords; i++) {
             Word word = new Word(defWords[i]);
-            wordsToFind[i] = word;
+            wordBank[i] = word;
         }
     }
 
     private void populate() {
         int count = 0;
-        for (Word word : wordsToFind) {
+        for (Word word : wordBank) {
             if (word.isInTable())
                 continue;
-            switch (word.getOrientation()) {
-                // case 0:
-                // for (HorizontalRow row : horizontalRows) {
-                // row.populate(word);
-                // }
-                // case 1:
-                // for (VerticalRow row : verticalRows) {
-                // row.populate(word);
-                // }
-                case 2:
-                    for (DiagonalRow row : leftDiagonalRows) {
-                        row.populate(word);
-                    }
+            for (int i = 0; i < 3; i++) {
+                switch (word.getOrientation()) {
+                    case 0:
+                        populateRandomly(horizontalRows, word);
+                        break;
+                    case 1:
+                        populateRandomly(verticalRows, word);
+                        break;
+                    case 2:
+                        populateRandomly(leftDiagonalRows, word);
+                        break;
+                    case 3:
+                        populateRandomly(rightDiagonalRows, word);
+                }
+                if (!word.isInTable())
+                    break;
             }
             if (!word.isInTable())
                 count++;
         }
+        wordsToFind = new Word[count];
+        int i = 0;
+        for (Word word : wordBank) {
+            if (!word.isInTable()) {
+                wordsToFind[i] = word;
+                i++;
+            }
+        }
         for (HorizontalRow row : horizontalRows) {
             row.fillTheRest();
+        }
+    }
+
+    public void populateRandomly(Row[] rows, Word word) {
+        Random rand = new Random();
+        int[] rowsIndexes = new int[rows.length];
+        for (int i = 0; i < rand.nextInt(100); i++) {
+            int index = rand.nextInt(rows.length);
+            for (int o = rand.nextInt(rows.length); o < rows.length; o += rand.nextInt(rows.length)) {
+                rowsIndexes[index] = o;
+            }
+        }
+        for (int i : rowsIndexes) {
+            rows[i].populate(word);
         }
     }
 
@@ -78,5 +107,16 @@ public class Table {
             row.print();
         }
         System.out.println("-----------------------------END-------------------------------");
+    }
+
+    public void printWordsToFind() {
+        System.out.print("Words to find: ");
+        for (Word word : wordsToFind) {
+            if (word.equals(wordsToFind[wordsToFind.length - 1])) {
+                System.out.printf("%s.", word.getWord());
+            } else
+                System.out.printf("%s, ", word.getWord());
+        }
+        System.out.println();
     }
 }
